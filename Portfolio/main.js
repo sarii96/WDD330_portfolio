a5273ab37b2b10ba551973e7d1d0e51665f3f7ec
sarii96 <sara-96325@hotmail.com>
2020-09-19 21:26:00 -0600

    hi

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,13 +1,23 @@
 const currentDate = new Date();
 console.log(currentDate);
 
-  const currentYear = currentDate.getFullYear();
-  console.log(currentYear);
+const currentYear = currentDate.getFullYear();
+console.log(currentYear);
+
+document.getElementById("currentYear").innerHTML =  currentYear;
+const links = [
+  {
+    label: "Week01",
+    url: "week1/week1.html"
+  },
+  {
+    label: "Week02",
+    url: "week1/week2.html"
+  }
   
-  document.getElementById("currentYear").innerHTML =  currentYear;
-  const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    }
-  ]
+];
+let htmlLinks = "";
+for (let linkObject of Object.values(links)) {
+  htmlLinks += '<li><a href="' + linkObject.url + '">' + linkObject.label + '</a> </li>';
+}
+document.getElementById("tableOfContents").innerHTML =htmlLinks ;
--- Portfolio/week1/main.js
+++ Portfolio/week1/main.js
@@ -5,3 +5,20 @@ console.log(currentDate);
   console.log(currentYear);
   
   document.getElementById("currentYear").innerHTML =  currentYear;
+
+  function loadStory(){
+     let storyName = document.getElementById("name_input").value;
+     let storyHTML = localStorage.getItem(storyName);
+     document.getElementById("story_editor").value = storyHTML;
+    }
+    function saveStory(){
+   let storyName = document.getElementById("name_input").value;
+   let storyHTML = document.getElementById("story_editor").value;
+    localStorage.setItem(storyName, storyHTML);
+    }
+   function displayStory(){
+    let storyHTML = document.getElementById("story_editor").value;
+   
+    document.getElementById("story_display").innerHTML = storyHTML;
+   
+   }
